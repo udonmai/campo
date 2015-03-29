034618469d43637e22078eda380e5932bb0c1c07
udonmai <udonmai@gmail.com>
2015-03-30 01:30:16 +0900

    local 3 30

--- db/migrate/201411110931110_remove_lower_column.rb
+++ db/migrate/201411110931110_remove_lower_column.rb
@@ -1,6 +1,6 @@
 class RemoveLowerColumn < ActiveRecord::Migration
   def change
-    remove_column :users, :username_lower
+    #remove_column :users, :username_lower
     remove_column :users, :email_lower
     remove_index :users, :username
     remove_index :users, :email
